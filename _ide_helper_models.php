77349c1c2a47cb4eaf66bc02e9c599e81687f2d8
joaopedro735 <jpfcarreira@gmail.com>
2018-11-16 19:25:25 +0000

    Initial commit

---
+++ _ide_helper_models.php
@@ -0,0 +1,21 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ */
+	class User extends \Eloquent {}
+}
+
